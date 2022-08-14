b99e6d6867a631ccbc175487b50330930d2a015c
Brock-99 <elmatadordelmar99@gmail.com>
2022-08-14 21:39:30 +0530

    Aded Values Dynamicaly Using props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from "react";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+    const expenses = [
+        {title:'Car Insurance', date: new Date(2022,8,14), amount:'12345' },
+        {title:'Tyre', date: new Date(2022,9,19), amount:'1234' },
+        {title:'Vehicle', date: new Date(2022,10,30), amount:'123456' },
+    ]
+
     return (
         <div>
             <h2>Let's Get Started</h2>
-            <ExpenseItem/>
+            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
+            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
         </div>
     );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import './ExpenseItem.css'
 import React from "react";
 
-function ExpenseItem() {
-    const date = new Date();
-    const title = "Car Insurance";
-    const amount = "12345";
+function ExpenseItem(props) {
 
     return <div className="expense-item">
-        <div>{date.toISOString()}</div>
-        <div className="expense-item__description"><h2>{title}</h2></div>
-        <div className="expense-item__price">${amount}</div>
+        <div>{props.date.toISOString()}</div>
+        <div className="expense-item__description"><h2>{props.title}</h2></div>
+        <div className="expense-item__price">${props.amount}</div>
     </div>
 }
 
